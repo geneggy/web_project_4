f7104e7a92dcdff3b924ee2607c6efae39402f57
geneggy <geneggy@gmail.com>
2020-05-24 23:01:40 -0700

    margin changes

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 //interaction buttons
-let editPopupButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__exit');
-let savePopupButton = document.querySelector('.popup__save-button');
-let popup = document.querySelector('.popup');
+const editPopupButton = document.querySelector(".profile__edit-button");
+const closePopupButton = document.querySelector(".popup__exit");
+const savePopupButton = document.querySelector(".popup__save-button");
+const popup = document.querySelector(".popup");
 
 //form and form fields
-let form = document.querySelector('.popup__form');
-let aboutInput = document.querySelector('.popup__form-about');
-let nameInput = document.querySelector('.popup__form-name');
+const form = document.querySelector(".popup__form");
+const aboutInput = document.querySelector(".popup__form-about");
+const nameInput = document.querySelector(".popup__form-name");
 
 //value fields
-let profileName = document.querySelector('.profile__name');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const profileName = document.querySelector(".profile__name");
+const profileSubtitle = document.querySelector(".profile__subtitle");
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileSubtitle.textContent = aboutInput.value;
-  popup.classList.toggle('popup_open');
+  popup.classList.toggle("popup_open");
 }
 
 function togglePopup() {
-  popup.classList.toggle('popup_open');
+  popup.classList.toggle("popup_open");
 }
 
-
-editPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click',togglePopup);
-form.addEventListener('submit', formSubmitHandler);
+editPopupButton.addEventListener("click", togglePopup);
+closePopupButton.addEventListener("click", togglePopup);
+form.addEventListener("submit", formSubmitHandler);
