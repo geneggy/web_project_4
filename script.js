d0a35a205e1c6a7fdf9e17c0090b293bbb671fd6
geneggy <geneggy@gmail.com>
2020-06-04 19:39:45 -0700

    feature: image modal implemented

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const editPopupButton = document.querySelector('.profile__edit-button');
 const closeEditPopupButton = document.querySelector('.popup__exit_edit');
 const closeAddPopupButton = document.querySelector('.popup__exit_new-place');
+const closeImgPopupButton = document.querySelector('.popup__exit_img');
 const addPopupButton = document.querySelector('.profile__add-button');
 const popupEdit = document.querySelector('.popup_edit');
 const popupPlace = document.querySelector('.popup_new-place');
@@ -14,50 +15,24 @@ const nameInput = document.querySelector('.popup__form-name');
 const titleInput = document.querySelector('.popup__form-title');
 const linkInput = document.querySelector('.popup__form-link');
 
-//value fields
+//profile fields to select
 const profileName = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
-//Edit profile functions
-function formSubmitHandler(e) {
-  e.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileSubtitle.textContent = aboutInput.value;
-  popupEdit.classList.toggle('popup_open');
-}
+//image modal fields
+const popupImage = document.querySelector('.popup__image');
+const popupSubtext = document.querySelector('.popup__subtext');
+const popupImgModal = document.querySelector('.popup_img');
 
-//add new image functions
+console.log(popupSubtext);
+console.log(popupImage);
+console.log(popupImgModal);
 
-function submitAddForm(e) {
-  e.preventDefault();
-  renderPlace({name: titleInput.value, link: linkInput.value});
-  addForm.reset();
-  togglePlacePopup();
-}
-
-
-//toggle popups
-function toggleEditPopup() {
-  popupEdit.classList.toggle('popup_open');
-}
-
-function togglePlacePopup() {
-  popupPlace.classList.toggle('popup_open');
-}
-
-editPopupButton.addEventListener('click', toggleEditPopup);
-addPopupButton.addEventListener('click', togglePlacePopup);
-closeEditPopupButton.addEventListener('click', toggleEditPopup);
-closeAddPopupButton.addEventListener('click', togglePlacePopup);
-
-addForm.addEventListener('submit', submitAddForm);
-editForm.addEventListener('submit', formSubmitHandler);
-
-
-//initial 6 places
+//template for new places and container for places
 const placeTemplate = document.querySelector('#placeTemplate').content;
 const placesContainer = document.querySelector('.places');
 
+//initial 6 places
 const initialPlaces = [
   {
     name: 'Yosemite Valley',
@@ -85,7 +60,24 @@ const initialPlaces = [
   },
 ];
 
-//create a place
+
+//Edit profile functions
+function formSubmitHandler(e) {
+  e.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileSubtitle.textContent = aboutInput.value;
+  popupEdit.classList.toggle('popup_open');
+}
+
+//add new image functions
+function submitAddForm(e) {
+  e.preventDefault();
+  createPlace({name: titleInput.value, link: linkInput.value});
+  addForm.reset();
+  togglePlacePopup();
+}
+
+//create a place and render it in page
 function createPlace(place) {
   const placeElement = placeTemplate.cloneNode(true);
   const placeImage = placeElement.querySelector('.place__image');
@@ -99,7 +91,6 @@ function createPlace(place) {
   placeTitle.textContent = place.name;
 
   //event listeners
-
   //toggle heart fill in on click
   placeLike.addEventListener('click', function (e) {
     e.target.classList.toggle('place__like-button_liked');
@@ -110,18 +101,45 @@ function createPlace(place) {
     e.target.parentElement.remove();
   });
 
-  return placeElement;
+
+  //image modal
+  placeImage.addEventListener('click', function () {
+    popupImage.src = place.link;
+    popupSubtext.textContent = place.name;
+    popupImgModal.classList.toggle('popup_open');
+  });
+
+  //add new element to container
+  placesContainer.append(placeElement);
 }
 
-//render place
-function renderPlace(place) {
-  placesContainer.append(createPlace(place));
+//popup toggle functions
+function toggleEditPopup() {
+  popupEdit.classList.toggle('popup_open');
 }
 
-//render each place on initial render
+function togglePlacePopup() {
+  popupPlace.classList.toggle('popup_open');
+}
 
-initialPlaces.forEach(function (place) {
-  renderPlace(place);
+function toggleImgPopup() {
+  popupImgModal.classList.toggle('popup_open');
+}
+
+
+//render all initial places on load
+initialPlaces.forEach(function(place) {
+  createPlace(place);
 });
+
+//event listeners
+editPopupButton.addEventListener('click', toggleEditPopup);
+addPopupButton.addEventListener('click', togglePlacePopup);
+closeEditPopupButton.addEventListener('click', toggleEditPopup);
+closeAddPopupButton.addEventListener('click', togglePlacePopup);
+closeImgPopupButton.addEventListener('click', toggleImgPopup);
+addForm.addEventListener('submit', submitAddForm);
+editForm.addEventListener('submit', formSubmitHandler);
+
 
 
