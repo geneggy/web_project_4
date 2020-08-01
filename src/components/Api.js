da751697772e8398317a0ad6fba7d9ff00b912a4
geneggy <geneggy@gmail.com>
2020-07-31 19:31:02 -0700

    more api templating

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,37 +1,29 @@
+/* eslint-disable no-unused-vars */
+
+
 // Token: 84e6855b-5fd3-47bd-8833-73dfffbf3da7
 // Group ID: group-3
 //https://around.nomoreparties.co/v1/group-3
 //token goes inside headers
 
-//fetch cards
-//https://around.nomoreparties.co/v1/groupId/cards
-
-//edit user infp
-//PATCH https://around.nomoreparties.co/v1/groupId/users/me
-
-//Add Card
-//POST https://around.nomoreparties.co/v1/groupId/cards
-
-//Delete Card
-//DELETE https://around.nomoreparties.co/v1/groupId/cards/cardId
-//DELETE https://around.nomoreparties.co/v1/groupId/cards/5d1f0611d321eb4bdcd707dd
-
-//adding/removing likes
-//PUT https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
-//DELETE https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
-
-//update user pic
-//PATCH https://around.nomoreparties.co/v1/groupId/users/me/avatar
+// const api = new Api({
+//   baseUrl: "https://around.nomoreparties.co/v1/group-3",
+//   headers: {
+//     authorization: "84e6855b-5fd3-47bd-8833-73dfffbf3da7",
+//     "Content-Type": "application/json"
+//   }
+// });
 
 export default class Api {
-  constructor({url, headers, token}) {
-    this._url = url;
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
     this._headers = headers;
-    this._token = token;
   }
 
-  getInitialCards() {
-    return fetch(`${this.url}/cards`, {
+
+  //https://around.nomoreparties.co/v1/groupId/card
+  getCardList() {
+    return fetch(`${this.baseUrl}/cards`, {
       headers: this._headers
     })
       .then(res => {
@@ -41,4 +33,39 @@ export default class Api {
         // if the server returns an error, reject the promise
         return Promise.reject(`Error: ${res.status}`);
       });
-  }}
+  }
+
+  // get user info
+  getUserInfo() {
+    return fetch(`${this.baseUrl}/users/me`, {
+      headers: this.headers
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        // if the server returns an error, reject the promise
+        return Promise.reject(`Error: ${res.status}`);
+      });
+  }
+
+  getAppInfo() {}
+
+  //POST https://around.nomoreparties.co/v1/groupId/cards
+  addCard({ name, link}) {}
+
+  //DELETE https://around.nomoreparties.co/v1/groupId/cards/cardId
+  //DELETE https://around.nomoreparties.co/v1/groupId/cards/5d1f0611d321eb4bdcd707dd
+  removeCard(cardId) {}
+
+  //PUT https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
+  //DELETE https://around.nomoreparties.co/v1/groupId/cards/likes/cardId
+  changeLikeCardStatus(cardId, like) {}
+  
+  //PATCH https://around.nomoreparties.co/v1/groupId/users/me
+  setUserInfo() {}
+
+  //PATCH https://around.nomoreparties.co/v1/groupId/users/me/avatar
+  setUserAvatar({avatar}) {}
+
+}
